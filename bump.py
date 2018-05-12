d884943382862edfc87ab8ce8abb1588e538c959
Gregoire Deletang <gregoire.deletang@polytechnique.edu>
2018-05-12 12:13:40 -0400

    Adding proxy option

--- bump.py
+++ bump.py
@@ -1,8 +1,8 @@
 from scapy.all import *
 import threading
 
-def sendMessage(ip, message):
-    ping = IP(dst="google.com", src=ip)/ICMP(id=2000)/message
+def sendMessage(ip, message, proxy):
+    ping = IP(dst=proxy, src=ip)/ICMP(id=2000)/message
     send(ping)
 
 def receivePacket(packet):
@@ -22,8 +22,9 @@ import time
 time.sleep(1)
 
 dest = "192.168.0.29"
+proxy = "192.168.0.17"
 
 while True:
     message = raw_input("Entrez votre message : ")
-    sendMessage(dest, message)
+    sendMessage(dest, message, proxy)
     continue;
